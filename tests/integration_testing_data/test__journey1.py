c565ed4ba4aa83aaa74878cc54724c83e3b5bafa
sezell <sezell@alteryx.com>
2023-05-28 14:58:41 -0600

    fixing linting for dummy test file

--- tests/integration_testing_data/test__journey1.py
+++ tests/integration_testing_data/test__journey1.py
@@ -3,25 +3,26 @@ import logging
 from locust import between, task
 
 from grasshopper.lib.grasshopper import BaseJourney, Grasshopper
-from grasshopper.lib.util.utils import custom_trend, check
+from grasshopper.lib.util.utils import check, custom_trend
 
 logger = logging.getLogger(__name__)
 
 
 class Journey1(BaseJourney):
     wait_time = between(min_wait=1, max_wait=2)
-    defaults = {"thresholds": {
-        "PX_TREND_google_home": {"type": "custom", "limit": 200},
-        "google_home": {"type": "custom", "limit": 200}}}
+    defaults = {
+        "thresholds": {
+            "PX_TREND_google_home": {"type": "custom", "limit": 200},
+            "google_home": {"type": "custom", "limit": 200},
+        }
+    }
 
     @task
     @custom_trend("PX_TREND_google_home")
     def journey1_task(self):
         logger.info(f"VU {self.vu_number}: Starting journey1_task")
         response = self.client.get("https://google.com", name="google_home")
-        check("Status code is good",
-              response.status_code < 400,
-              self.environment)
+        check("Status code is good", response.status_code < 400, self.environment)
         logger.info(f"VU {self.vu_number}: Google result: {response.status_code}")
 
 
