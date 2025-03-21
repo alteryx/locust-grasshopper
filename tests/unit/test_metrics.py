3e66097c81f9ed871631874efbcaba24d9811539
meenakshi.kumar <Meenakshi.kumar@alteryx.com>
2025-03-21 18:51:31 +0530

    ruff formats

--- tests/unit/test_metrics.py
+++ tests/unit/test_metrics.py
@@ -47,6 +47,11 @@ def check_iteration_count(journey, count):
 
 
 def test__count_iterations(sample_func, mock_journey):
+    # simulate applying our decorator, remember that
+    # @decorator
+    # def method(...): ...
+    # is syntactic sugar for replace method with decorator(method)
+    # therefore to call method, you would do (decorator(method))(...)
     wrapped_func = count_iterations(sample_func)
     assert callable(wrapped_func), "Return value of decorator is not a callable."
     result_of_calling_wrapped_func = wrapped_func(mock_journey)
@@ -64,6 +69,8 @@ def test__count_iterations_multiple_calls(sample_func, mock_journey):
 def test__count_iterations_multiple_functions(
     sample_func, sample_func_different, mock_journey
 ):
+    # check that multiple wraps (represents different tasks on same journey) can both
+    # increment the vu_iteration count correctly
     wrapped_func = count_iterations(sample_func)
     wrapped_func(mock_journey)
     wrapped_diff = count_iterations(sample_func_different)
@@ -76,11 +83,20 @@ def test__task_default_weight(sample_func, mock_journey):
     wrapped_func = task(sample_func)
     assert callable(wrapped_func), "Return value of decorator is not a callable."
     result_of_calling_wrapped_func = wrapped_func(mock_journey)
+    # our task decorator wraps locust's task decorator with vu_iteration tracking
+    # check that the locust_task_weight attr was transferred to the new wrapped function
+    # this attr is how locust tells which tasks are to be collected when running a test
     assert wrapped_func.locust_task_weight == 1
     check_iteration_count(result_of_calling_wrapped_func, 1)
 
 
 def test__task_specifying_weight(sample_func):
+    # use this class to check specifying the decorator with a weight
+    # there should be a way to check using the function equivalent, but I couldn't
+    # quite get the mocking correct in that case, since both our code and the locust
+    # code are assuming that the task is a bound method on the journey class
+    # the syntactic equivalent for the sugar @task(4) is
+    # the attribute fake_task = (task(4))(fake_task)
     class FakeJourney(BaseJourney):
         client_id = uuid4()
         vu_iteration = 0
